71d19c85db95c876b0518177a5722b8c9dbb5e4f
Isaac <exessgamer@gmail.com>
2025-09-25 15:08:51 -0600

    finishing tutorial app

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,14 +9,27 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
   public int Id { get; set; }
-  public string? Title { get; set; }
-  
+
+  [StringLength(60, MinimumLength = 3)]
+  [Required]
+  public string Title { get; set; } = string.Empty;
+
   [DisplayName("Release Date")]
   [DataType(DataType.Date)]
   public DateTime ReleaseDate { get; set; }
-  public string? Genre { get; set; }
+
+  [RegularExpression(@"^[A-Z]+[a-zA-Z\s-]*$")]
+  [Required]
+  [StringLength(30)]
+  public string Genre { get; set; } = string.Empty;
 
   // [Precision(18, 2)]
+  [Range(1, 100)]
+  [DataType(DataType.Currency)]
   [Column(TypeName = "decimal(18, 2)")]
   public decimal Price { get; set; }
+
+  [Range(1, 5)]
+  [Required]
+  public int Rating { get; set; } = 0;
 }
